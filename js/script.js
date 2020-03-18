d48b447b05fa19d90128fa31cca1f9ee7ea55295
Flucky <fluckymendes@gmail.com>
2020-03-18 14:57:24 +0300

    Reworked adaptive menu by jQuery, it works better than I would write by vanilla JS

--- js/script.js
+++ js/script.js
@@ -3,18 +3,67 @@ let mainMenu = document.querySelector(".main-menu__list");
 let menuCloseButton = document.querySelector(".main-menu__btn--close");
 
 // Закрытие главного меню
-menuCloseButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mainMenu.classList.add("visually-hidden");
-})
+// menuCloseButton.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     mainMenu.classList.add("display-none");
+// })
 
-// Открытие главного меню
-menuOpenButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mainMenu.classList.remove("visually-hidden");
-})
+// // Открытие главного меню
+// menuOpenButton.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     mainMenu.classList.remove("display-none");
+// })
 
-menuOpenButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mainMenu.classList.remove("visually-hidden");
-})
+
+menu = $('.main-menu__list');
+menuButtonOpen = $('.main-menu__btn--open');
+menuButtonClose = $('.main-menu__btn--close');
+
+// Прячем меню на мобильной версии если загрузился js 
+$(document).ready (function(){
+    var w = $(window).width(); 
+    if(w < 768) {
+        menu.hide();
+    }
+}); 
+
+// $(window).bind("load", function() {
+//     var w = $(window).width(); 
+//     if(w < 768) {
+//         menu.hide();
+//     }
+//  });
+
+$(function() {
+
+    // Открываем меню по кнопен "Бургер"
+    menuButtonOpen.on('click', function(e) {
+        e.preventDefault(); 
+        menu.slideToggle();
+    });
+  
+    // Показываем меню при увеличении ширины окна 
+    $(window).resize(function(){
+        var w = $(this).width(); 
+        if(w > 768 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+        }
+    });
+  
+    // Прячем меню при усеньшении ширины окна 
+    $(window).resize(function(){
+        var w = $(this).width(); 
+        if(w < 768) {
+        menu.hide();
+        }
+    });
+
+    // Закрываем меню по кнопке "Х"
+    menuButtonClose.on('click', function(e) {                
+        var w = $(window).width(); 
+        if(w < 768 ) {
+        menu.slideToggle(); 
+        }
+    });
+//   $('.open-menu').height($(window).height());
+});
